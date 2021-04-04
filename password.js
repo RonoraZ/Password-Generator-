22c33a4174385d0c6311d6053cd28ebda70fb4b8
Amalec Guzman <amalec.c.guzman@gmail.com>
2021-04-03 22:16:30 -0700

    password

--- password.js
+++ password.js
@@ -1,8 +1,9 @@
 const CharacterAmountnumber = document.getElementById 
 ('CharacterAmountnumber')  
 const IncludeUppercaseElement = document.getElementById('IncludeUppercase') 
-const includeNumbersElement = document.getElementById('IncludeNumberscase') 
-const includeSymbolsElement = document.getElementById('IncludeSymbolscase')
+const IncludeNumbersElement = document.getElementById('IncludeNumberscase') 
+console.log("IncludeNumbersElement", IncludeNumbersElement);
+const IncludeSymbolsElement = document.getElementById('IncludeSymbolscase')
 const form = document.getElementById('generator')  
 const passdisplay = document.getElementById('passdisplay')
 
@@ -15,24 +16,31 @@ CharacterAmountnumber.addEventListener('input',syncCharacterAmount)
 
 form.addEventListener('submit',e =>{ 
     e.preventDefault() 
-    const CharacterAmount = CharacterAmountnumber.checked
+    const CharacterAmount = CharacterAmountnumber.value
+    console.log("characterAmount",CharacterAmount);
     const includeUppercase =IncludeUppercaseElement.checked 
-    const IncludeNumbers = includeNumbersElement.checked 
-    const includeSymbols = includeSymbolsElement.checked
-    const password = generatePassword(CharacterAmount,includeUppercase, IncludeNumbers, includeSymbols)
+    const includeNumbers = IncludeNumbersElement.checked  
+    console.log("IncludeSymbolsElement",IncludeSymbolsElement);
+    const includeSymbols = IncludeSymbolsElement.checked
+    const password = generatePassword(CharacterAmount,includeUppercase, includeNumbers, includeSymbols)
+    console.log("password",password);
     passdisplay.innerText = password
 }) 
 
-function generatePassword(CharacterAmount, includeNumbersElement, inculudeSymbols, includeUppercase){ 
+function generatePassword(characterAmount, includeUppercase,includeNumberscase, includeSymbolscase, ){ 
+
     let charCodes = LoweCase_Char_Codes 
     if (includeUppercase) charCodes = charCodes.concat(UpperCase_Char_Codes)
-    if (includeNumbercase) charCodes = charCodes.concat(NumberCase_Char_Codes) 
+    if (includeNumberscase) charCodes = charCodes.concat(NumberCase_Char_Codes) 
     if (includeSymbolscase) charCodes = charCodes.concat(SymbolCase_Char_Codes)  
     const passwordCharacters = [] 
-    for (let i= 0; i< CharacterAmount; i++) {  
+    console.log("characterAmount",characterAmount);
+    for (let i= 0; i< characterAmount; i++) {  
         const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+        console.log("characterCode",characterCode);
         passwordCharacters.push(String.fromCharCode(characterCode))
     } 
+    console.log("passwordCharacters",passwordCharacters);
     return passwordCharacters.join('')
 } 
 
